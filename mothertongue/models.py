f3ada97766312c7c71043dba2df39040e7ab4101
Michał Sałaban <michal@salaban.info>
2010-11-10 16:46:06 +0100

    Added cache Removed trailing whitespaces

--- mothertongue/models.py
+++ mothertongue/models.py
@@ -1,4 +1,5 @@
 # import stuff we need from django
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils.translation import get_language, ugettext, ugettext_lazy as _
 from django.utils import translation
@@ -10,15 +11,21 @@ from django.conf import settings
 class MothertongueModelTranslate(models.Model):
     """
     Models that extend this class will have the ability to define:
-    - translation_set, e.g: 'contenttranslation_set', the many to many 
+    - translation_set, e.g: 'contenttranslation_set', the many to many
       manager name for the translation models for this model.
-    - translated_fields = e.g: ['content',] - the fields that may be 
-      translated. A match is found if a translation exists with a 'language' 
+    - translated_fields = e.g: ['content',] - the fields that may be
+      translated. A match is found if a translation exists with a 'language'
       the same as the current language, e.g: 'es' and a matching name.
     """
+    _translation_cache = None
+
     class Meta(object):
-        abstract = True   
-            
+        abstract = True
+
+    def __init__(self, *args, **kwargs):
+        super(MothertongueModelTranslate, self).__init__(*args, **kwargs)
+        self._translation_cache = {}
+
     def __getattribute__(self, name):
         """
         Specialise to look for translated content, note we use super's
@@ -27,13 +34,15 @@ class MothertongueModelTranslate(models.Model):
         get = lambda p:super(MothertongueModelTranslate, self).__getattribute__(p)
         translated_fields = get('translated_fields')
         if name in translated_fields:
-            try:                
+            try:
                 translation_set = get('translation_set')
                 code = translation.get_language()
                 translated_manager = get(translation_set)
-                translated_object = translated_manager.get(language=code)
+                translated_object = self._translation_cache.get(code,
+                        translated_manager.get(language=code))
+                self._translation_cache[code] = translated_object
                 return getattr(translated_object, name)
-            except Exception:
+            except (ObjectDoesNotExist, AttributeError):
                 return get(name)
         else:
             return get(name)
